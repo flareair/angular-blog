9fbf52297105ed9b0adb4f0f2b0f6092310a4c07
flareair <flareair0@gmail.com>
2016-04-06 12:57:37 +0300

    Added first e2e test of main page

--- client/tests.webpack.js
+++ client/tests.webpack.js
@@ -6,5 +6,5 @@
 import 'angular';
 import 'angular-mocks/angular-mocks';
 
-var testsContext = require.context(".", true, /.spec$/);
+var testsContext = require.context(".", true, /.spec.js$/);
 testsContext.keys().forEach(testsContext);
