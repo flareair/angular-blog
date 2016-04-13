94aa311490347b78a94eaa133e0443c3cbb38f5c
flareair <flareair0@gmail.com>
2016-04-13 11:16:02 +0300

    Added client side tests for 404 page

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,7 +3,8 @@
 exports.config = {
     framework: 'mocha',
     specs: [
-        'client/pages/*.spec.e2e.js'
+        'client/pages/*.spec.e2e.js',
+        'client/shared/*.spec.e2e.js',
     ],
     seleniumAddress: 'http://localhost:4444/wd/hub',
     baseUrl: 'http://localhost:8080/',
